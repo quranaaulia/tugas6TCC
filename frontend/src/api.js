a8d2ea590cbd35d3f005b21232b353072a1348a1
quranaaulia <162184412+quranaaulia@users.noreply.github.com>
2025-04-17 23:08:58 +0700

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://notes-fe183-dot-f-01-450707.uc.r.appspot.com/";
+const API_URL = "https://notes-be183-651881126563.us-central1.run.app/api/notes";
 
 export const getNotes = async () => {
   const response = await axios.get(API_URL);
